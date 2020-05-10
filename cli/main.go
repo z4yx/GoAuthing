5e67be4e79a953f7a82de5154a5991b35f54b7a6
z4yx <z4yx@users.noreply.github.com>
2020-05-10 11:27:11 +0800

    Bump version to 1.5.3

--- cli/main.go
+++ cli/main.go
@@ -337,7 +337,7 @@ func main() {
 	 auth-thu [options] login
 	 auth-thu [options] logout`,
 		Usage:    "Authenticating utility for Tsinghua",
-		Version:  "1.5.2",
+		Version:  "1.5.3",
 		HideHelp: true,
 		Flags: []cli.Flag{
 			&cli.StringFlag{Name: "username, u", Usage: "your TUNET account `name`"},
