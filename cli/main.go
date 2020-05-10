4897bd1a277f41f4150d0e75dfd0931daea0790d
z4yx <z4yx@users.noreply.github.com>
2020-05-10 11:25:52 +0800

    reduce data usage

--- cli/main.go
+++ cli/main.go
@@ -198,8 +198,8 @@ func keepAliveLoop(c *cli.Context, campusOnly bool) (ret error) {
 			fmt.Printf("Failed to access %s, you have to re-login.\n", v4Target)
 			break
 		}
-		// Consumes ~120MB per day
-		time.Sleep(45 * time.Second)
+		// Consumes ~100MB per day
+		time.Sleep(55 * time.Second)
 	}
 	return
 }
