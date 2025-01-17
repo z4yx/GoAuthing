9d85e38372dd099b8246330e2bfedbac14a81930
Jiajie Chen <c@jia.je>
2025-01-17 08:58:03 +0800

    Release v2.3.1

--- cli/main.go
+++ cli/main.go
@@ -397,7 +397,7 @@ func main() {
 	 auth-thu [options] logout
 	 auth-thu [options] online [online_options]`,
 		Usage:    "Authenticating utility for Tsinghua",
-		Version:  "2.3",
+		Version:  "2.3.1",
 		HideHelp: true,
 		Flags: []cli.Flag{
 			&cli.StringFlag{Name: "username, u", Usage: "your TUNET account `name`"},
